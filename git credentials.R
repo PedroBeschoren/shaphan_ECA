6fa7751ad9789fcc75600871af1ee0c85735c97d
Shaphan Chia <shaphanyong@gmail.com>
2023-05-15 16:32:54 +0200

    edits and new word and PPt files

---
+++ git credentials.R
@@ -0,0 +1,13 @@
+usethis::create_github_token() #
+
+## Follow either of the two to access git credentials
+install.packages("gitcreds") # R package for accessing the Git credential store:
+credentials::set_github_pat() # accessing the Git credential store:
+
+gitcreds::gitcreds_set()
+
+
+install.packages("credentials")
+
+
+
